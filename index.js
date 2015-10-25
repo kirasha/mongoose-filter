f25781b700e02003c54138325a572d1b663887ca
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-10-25 02:14:14 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,237 @@
+/*
+ * Mongoose REST Query Plugin
+ * **************************
+ * An opinionated plugin for handling REST Queries in Mongoose
+ * @author Olivier.dusabimana <diokey.olivier@gmail.com>, Twitter: @diokey
+ * MIT License
+ */
+
+function filterPlugin(schema, options) {
+
+  /*
+   * options: {
+   *   fields: [], // default: all,
+   *   pagination: {
+   *     page: 1 // default 0,
+   *     size: 30 // default 30
+   *   },
+   *   filters: [], // filters: array, default [],
+   *   sort: [], // sort properties default []
+   *   embed: [] // embedded objects: It could be array, String, Object notation
+   * }
+   *
+   */
+
+
+  function parseFields(inputs) {
+    return inputs.join(' ');
+  }
+
+  function parseEmbeded(props) {
+    if (!props || !Array.isArray(props)) {
+      throw new Error('Expected array of docs to populate');
+    }
+
+    if (!props) {
+      return;
+    }
+
+    var docMap={};
+    var docum = {};
+    docum.fields = [];
+
+    props.forEach(function(prop) {
+      var dotIndex = prop.indexOf('.');
+      if (dotIndex === -1) {
+        docum.doc = prop;
+        docum.fields = null;
+      } else {
+        docum.doc = prop.substr(0, dotIndex);
+        docum.fields.push(prop.substr(dotIndex + 1));
+      }
+      docMap[docum.doc] = docum;
+    });
+
+    var keys = Object.keys(docMap);
+    return keys.map(function(key) {
+      return docMap[key];
+    });
+
+  }
+
+  function parseSort(inputs) {
+    if (!inputs) {
+      return;
+    }
+    var output = {};
+    var key;
+    var value;
+    var sortSign;
+    inputs.forEach(function(order) {
+      key='';
+      value='';
+      sortSign='';
+
+      if (typeof order === 'string') {
+        sortSign = order.substr(0,1);
+        if (sortSign === '-') {
+          key = order.substr(1);
+          value = -1;
+        } else {
+          key = order;
+          value = 1;
+        }
+        output[key] = value;
+      } else if (typeof order === 'object') {
+        key = Object.keys(order)[0];
+        output[key] = order[key];
+      }
+    });
+
+    return output;
+  }
+
+  function parseFilters(inputs) {
+
+    var filter = {};
+    var regex;
+
+    inputs.forEach(function(input) {
+      switch(input.operator) {
+        case '==':
+          filter[input.key] = input.value;
+          break;
+        case '!=':
+          filter[input.key] = {$ne: input.value};
+          break;
+        case '~':
+          try {
+            regex = new RegExp(input.value, 'i');
+          } catch (err) {
+            throw new Error('Invalid RegExp' + input.value);
+          }
+          if (regex) {
+            filter[input.key] = regex;
+          }
+          break;
+        case '!~':
+          try {
+            regex = new RegExp('^((?!'+input.value+').)*$', 'i');
+          } catch (err) {
+            throw new Error('Invalid RegExp' + input.value);
+          }
+          if (regex) {
+            filter[input.key] = regex;
+          }
+          break;
+        case '<':
+          filter[input.key] = {$lt: input.value};
+          break;
+        case '<=':
+          filter[input.key] = {$lte: input.value};
+          break;
+        case '>':
+          filter[input.key] = {$gt: input.value};
+          break;
+        case '>=':
+          filter[input.key] = {$gte: input.value};
+          break;
+        case 'in':
+          if(!Array.isArray(input.value)) {
+            throw new Error('like operator requires an array of values');
+          }
+          filter[input.key] = {$in: input.value};
+          break;
+        case 'between':
+          if(!Array.isArray(input.value) || input.value.length !== 2 ) {
+            throw new Error('Expected 2 values for between Operator');
+          }
+          filter[input.key] = {$gte: input.value[0], $lte: input.value[1]};
+          break;
+        case 'not between':
+          if(!Array.isArray(input.value) || input.value.length !== 2 ) {
+            throw new Error('Expected 2 values for not between Operator');
+          }
+          var lt = {};
+          var gt = {};
+          lt[input.key] = {$lt: input.value[0]};
+          gt[input.key] = {$gt: input.value[1]};
+          filter.$or = [lt , gt];
+          break;
+        default: throw new Error('Not Supported Operator' + input.operator);
+      }
+    });
+
+    return filter;
+  }
+
+  schema.statics.filter = function(conditions, callback) {
+    if (arguments.length < 2) {
+      if (typeof conditions ==='function') {
+        callback = conditions;
+        conditions = {};
+      } else {
+        callback = undefined;
+        if (!conditions) {
+          conditions = {};
+        }
+      }
+    }
+
+    conditions.fields           = conditions.fields || [];
+    conditions.pagination       = conditions.pagination || {};
+    conditions.pagination.page  = conditions.pagination.page || 1;
+    conditions.pagination.size = conditions.pagination.size || 30;
+    conditions.filters          = conditions.filters || [];
+    conditions.sort             = conditions.sort || [];
+    conditions.embed            = conditions.embed || [];
+
+    var query = null;
+
+    if(! conditions.filters.length) {
+      query = this.find();
+    } else {
+      var filters = parseFilters(conditions.filters);
+      query = this.find(filters);
+    }
+
+    if (conditions.fields) {
+      var fields = parseFields(conditions.fields);
+      query.select(fields);
+    }
+
+    if (conditions.embed) {
+      var docs = parseEmbeded(conditions.embed);
+      if (docs) {
+        docs.forEach(function(doc) {
+          if(doc.fields) {
+            query.populate(doc.doc, doc.fields.join(' '));
+          } else {
+            query.populate(doc.doc);
+          }
+        });
+      }
+    }
+
+    if (conditions.sort) {
+      var sortOrder = parseSort(conditions.sort);
+      query.sort(sortOrder);
+    }
+
+    query.limit(conditions.pagination.size);
+
+    var offset = (conditions.pagination.page - 1) * conditions.pagination.size;
+
+    query.skip(offset);
+
+    if (!callback) {
+      return query;
+    } else {
+      query.exec(callback);
+    }
+
+  };
+
+}
+
+module.exports = filterPlugin;
---
+++ tests/index.spec.js
@@ -0,0 +1,564 @@
+'use strict';
+
+var should        = require('should'),
+    mongoose      = require('mongoose'),
+    Schema        = mongoose.Schema,
+    FilterPlugin  = require('../');
+
+var PermissionSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: 'Permission name is required'
+  },
+  description: String,
+  active: {
+    type: Boolean,
+    default: true
+  }
+});
+
+var Permission = mongoose.model('Permission', PermissionSchema);
+
+var RoleSchema  = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: 'Role name is required'
+  },
+  description: String,
+  active: {
+    type: Boolean,
+    default: true
+  },
+  points: {
+    type: Number,
+    default: 0
+  },
+  permissions: [{
+    type: Schema.Types.ObjectId, ref: 'Permission'
+  }],
+  created: {
+    type: Date,
+    default: new Date()
+  }
+});
+
+RoleSchema.plugin(FilterPlugin);
+var Role = mongoose.model('Role', RoleSchema);
+
+function uniqueId() {
+  var x = Math.random() * new Date().getUTCMilliseconds();
+  var y = Math.random() * new Date().getUTCMilliseconds();
+  var z = Math.random() * new Date().getUTCMilliseconds();
+  var id = x * y / z;
+  return id;
+}
+
+function createRole(name, description) {
+  var points = Math.floor(uniqueId());
+  var role = new Role({
+    name: name,
+    description: description,
+    active: true,
+    points: points
+  });
+
+  return role;
+}
+
+function createPermission(name, description) {
+  var permission = new Permission({
+      name: name,
+      description: description,
+      active: true
+    });
+
+  return permission;
+}
+
+function connectDB() {
+  return new Promise(function(resolve, reject) {
+    mongoose.connect('mongodb://localhost/mongoose-filter');
+    mongoose.connection.on('error', function(err) {
+      reject(err);
+    });
+
+    mongoose.connection.on('connected', function() {
+      resolve();
+    });
+
+  });
+}
+
+
+function generatePermission(n) {
+  n = n || 50;
+  var i = 1;
+  var promises = [];
+  var promise;
+  while (i <= n) {
+    var suffix = uniqueId();
+    var perm = createPermission('Permission ' + suffix,
+                                'Permission Description ' + suffix);
+    promise = perm.save();
+    promises.push(promise);
+    i++;
+  }
+
+  return Promise.all(promises);
+}
+
+function saveRole(permissions) {
+  var suffix = uniqueId();
+  var role = createRole('Role ' + suffix, 'Role Description ' + suffix);
+  permissions.forEach(function(permission) {
+    role.permissions.push(permission.id);
+  });
+
+  return role.save().then(function(r) {
+    return r;
+  });
+}
+
+function generateRole(n) {
+  n = n || 50;
+  var i = 1;
+  var promise;
+  var promises = [];
+
+  while (i <= n) {
+    promise = generatePermission(5).then(saveRole);
+    promises.push(promise);
+    i++;
+  }
+  return Promise.all(promises);
+}
+
+var allRoles = [];
+function generateData() {
+  return generateRole(10).then(function(roles) {
+    allRoles = roles;
+    console.log('Done generating ' + allRoles.length + ' Role');
+  });
+}
+
+before(function(done) {
+  connectDB()
+  .then(generateData)
+  .then(done);
+});
+
+after(function(done) {
+  mongoose.connection.db.dropDatabase();
+  mongoose.connection.close(function() {
+    done();
+  });
+});
+
+describe('Mongoose Search Plugin', function() {
+  it('should expose a static filter function', function(done) {
+    Role.should.be.an.instanceOf(Object).and.have.property('filter');
+    'function'.should.equal(typeof(Role.filter));
+    done();
+  });
+
+  it('should match all rows if no parameter given', function(done) {
+    Role.filter(function(err, roles) {
+      should.not.exist(err);
+      should.exist(roles);
+      roles.length.should.equal(allRoles.length);
+      done();
+    });
+  });
+
+  it('should return a promise if not callback was passed', function(done) {
+    Role.filter().then(function(roles) {
+      should.exist(roles);
+      roles.length.should.equal(allRoles.length);
+      done();
+    });
+  });
+
+  it('should return only paged result', function(done) {
+    var options = {
+      pagination: {
+        page: 2,
+        size: 3
+      }
+    };
+
+    Role.filter(options).then(function(roles) {
+      should.exist(roles);
+      roles.length.should.equal(options.pagination.size);
+      var firstExpectedItem = allRoles[options.pagination.size];
+      var firstOutcomeItem = roles[0];
+      firstExpectedItem.name.should.equal(firstOutcomeItem.name);
+      done();
+    });
+  });
+
+  it('should return only specified fields', function(done) {
+    var options = {
+      fields: ['name', 'description']
+    };
+
+    Role.filter(options).then(function(roles) {
+      should.exist(roles);
+      roles.length.should.equal(allRoles.length);
+      var role = roles[0];
+      role.should.have.properties(options.fields);
+      should.not.exist(role.permissions);
+      should.not.exist(role.active);
+      done();
+    });
+  });
+
+  it('should sort on specified properties', function(done) {
+    var options = {
+      sort: ['name','-description']
+    };
+
+    Role.filter(options).then(function(roles) {
+      should.exist(roles);
+      var sortedRoles = allRoles.concat().sort(function(a, b) {
+
+        if (a.name === b.name) {
+          if (a.description < b.description) {
+            return 1;
+          }
+          if (a.description > b.description) {
+            return -1;
+          }
+          return 0;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+      });
+
+      sortedRoles[0].name.should.equal(roles[0].name);
+      sortedRoles[sortedRoles.length - 1].name.should.equal(
+        roles[roles.length - 1].name);
+      done();
+    });
+  });
+
+  describe('filter feature', function() {
+    it('should be able to filter on = Sign', function(done) {
+      var role = allRoles[0];
+      var filterObject = {
+        'filters': [
+          {
+            key: 'name',
+            operator: '==',
+            value: role.name
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        roles.length.should.equal(1);
+        roles[0].name.should.equal(role.name);
+        done();
+      });
+    });
+
+    it('should be able to filter on != Sign', function(done) {
+      var role = allRoles[0];
+      var filterObject = {
+        'filters': [
+          {
+            key: 'name',
+            operator: '!=',
+            value: role.name
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        roles.length.should.equal(allRoles.length - 1);
+        roles[0].name.should.not.equal(role.name);
+        done();
+      });
+    });
+
+    it('should be able to filter on ~ sign', function(done) {
+      var role = allRoles[0];
+      var roleName = role.name.split('.')[0];
+      var similarRoles = allRoles.filter(function(role) {
+        return role.name.split('.')[0] === roleName;
+      });
+
+      var filterObject = {
+        'filters': [
+          {
+            key: 'name',
+            operator: '~',
+            value: roleName
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        roles.length.should.equal(similarRoles.length);
+        done();
+      });
+    });
+
+    it('should be able to filter on !~ sign', function(done) {
+      var role = allRoles[0];
+      var roleName = role.name.split('.')[0];
+      var unlikeRoles = allRoles.filter(function(role) {
+        return role.name.split('.')[0] !== roleName;
+      });
+
+      var filterObject = {
+        'filters': [
+          {
+            key: 'name',
+            operator: '!~',
+            value: roleName
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        roles.length.should.equal(unlikeRoles.length);
+        done();
+      });
+    });
+
+    it('should be able to filter on < Sign', function(done) {
+      var role1 = allRoles[2];
+      var role2 = allRoles[5];
+      var points = Math.max(role1.points, role2.points);
+      var filterObject = {
+        'filters': [
+          {
+            key: 'points',
+            operator: '<',
+            value: points
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        roles.length.should.aboveOrEqual(1);
+        var expected = roles[0].points < points;
+        expected.should.equal(true);
+        done();
+      });
+    });
+
+    it('should be able to filter on <= Sign', function(done) {
+      var role1 = allRoles[2];
+      var role2 = allRoles[5];
+      var points = Math.max(role1.points, role2.points);
+      var filterObject = {
+        'filters': [
+          {
+            key: 'points',
+            operator: '<=',
+            value: points
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        roles.length.should.aboveOrEqual(2);
+        var expected = roles[0].points <= points;
+        expected.should.equal(true);
+        done();
+      });
+    });
+
+    it('should be able to filter on > Sign', function(done) {
+      var role1 = allRoles[2];
+      var role2 = allRoles[5];
+      var points = Math.min(role1.points, role2.points);
+      var filterObject = {
+        'filters': [
+          {
+            key: 'points',
+            operator: '>',
+            value: points
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        roles.length.should.aboveOrEqual(1);
+        var expected = roles[roles.length - 1].points > points;
+        expected.should.equal(true);
+        done();
+      });
+    });
+
+    it('should be able to filter on >= Sign', function(done) {
+      var role1 = allRoles[2];
+      var role2 = allRoles[5];
+      var points = Math.min(role1.points, role2.points);
+      var filterObject = {
+        'filters': [
+          {
+            key: 'points',
+            operator: '>=',
+            value: points
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        roles.length.should.aboveOrEqual(2);
+        var expected = roles[roles.length - 1].points >= points;
+        expected.should.equal(true);
+        done();
+      });
+    });
+
+    it('should be able to filter with "in" operator', function(done) {
+      var filterObject = {
+        'filters': [
+          {
+            key: 'name',
+            operator: 'in',
+            value: [allRoles[0].name, allRoles[4].name, allRoles[9].name]
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        roles.length.should.equal(3);
+        done();
+      });
+    });
+
+    it('should be able to filter with "between" operator', function(done) {
+
+      var limitLeft = Math.min(allRoles[3].points, allRoles[6].points);
+      var limitRight = Math.max(allRoles[3].points, allRoles[6].points);
+
+      var filterObject = {
+        'filters': [
+          {
+            key: 'points',
+            operator: 'between',
+            value: [limitLeft, limitRight]
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        roles.length.should.aboveOrEqual(2, 'Should return at least 2 roles');
+        var expected = (roles[0].points >= limitLeft && roles[0].points <= limitRight);
+        expected.should.equal(true);
+        done();
+      });
+    });
+
+    it('should be able to filter with "not between" operator', function(done) {
+
+      var limitLeft = Math.min(allRoles[3].points, allRoles[6].points);
+      var limitRight = Math.max(allRoles[3].points, allRoles[6].points);
+
+      var filterObject = {
+        'filters': [
+          {
+            key: 'points',
+            operator: 'not between',
+            value: [limitLeft, limitRight]
+          }
+        ]
+      };
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        var expected = (roles[0].points < limitLeft || roles[0].points > limitRight);
+        expected.should.equal(true);
+        done();
+      });
+    });
+
+    it('should be able to filter even with multiple filters', function(done) {
+
+      var limitLeft = Math.min(allRoles[3].points, allRoles[6].points);
+      var limitRight = Math.max(allRoles[3].points, allRoles[6].points);
+
+      var filterObject = {
+        'filters': [
+          {
+            key: 'name',
+            operator: '~',
+            value: 'Role 1'
+          },
+          {
+            key: 'points',
+            operator: 'between',
+            value: [limitLeft, limitRight]
+          }
+        ]
+      };
+
+      var expectedResult = allRoles.filter(function(role) {
+        var regex = new RegExp(filterObject.filters[0].value);
+        return (role.points >= limitLeft &&
+                role.points <= limitRight &&
+                regex.test(role.name));
+      });
+
+      Role.filter(filterObject).then(function(roles) {
+        should.exist(roles);
+        expectedResult.length.should.equal(roles.length);
+        done();
+      });
+    });
+
+  });
+
+  describe('Embed documents', function() {
+    it('should populate specified documents', function(done) {
+      var embeded = ['permissions'];
+
+      Role.filter({'embed': embeded}, function(err, roles) {
+        should.exist(roles);
+        roles[0].should.have.property('permissions');
+        var permissions = roles[0].permissions;
+        permissions.should.be.an.Array();
+        var permission = permissions[0];
+        permission.should.have.properties(['name','description','active']);
+        done();
+      });
+    });
+
+    it('should populate only fields of specified in the documents', function(done) {
+      var embeded = ['permissions.name','permissions.description'];
+
+      Role.filter({'embed': embeded}, function(err, roles) {
+        should.exist(roles);
+        roles[0].should.have.property('permissions');
+        var permissions = roles[0].permissions;
+        permissions.should.be.an.Array();
+        var permission = permissions[0];
+        permission.should.have.properties(['name','description']);
+        done();
+      });
+    });
+
+  });
+
+});
+
